61961b39bc0daff17ae15dad37d065b961e53a74
codinci <dkibau@gmail.com>
2022-08-04 09:36:17 +0300

    Few Corrections

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const names = ["Ada", "Brendan", "Ali"];
 function writeCards(names, event) {
     const messages = [];
     for(let i = 0; i < names.length; i++ ) {
-        messages.push(`"Thank you, ${names[i]}, for the wonderful ${event} gift!"`);
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
     }
     return messages;
 }
